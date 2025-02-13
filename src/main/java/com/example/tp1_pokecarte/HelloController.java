cfda6dae0d5ce4f6736ce4d392be83d360461756
Erwan "Bison" Thoury <55542386+erwanthoury@users.noreply.github.com>
2025-02-13 08:39:57 +0100

    aaa

--- src/main/java/com/example/tp1_pokecarte/HelloController.java
+++ src/main/java/com/example/tp1_pokecarte/HelloController.java
@@ -44,6 +44,7 @@ public class HelloController implements Initializable {
     private Label lblLblSort2;
     @FXML
     private Label lblLblSort1;
+    public Carte c;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -77,38 +78,78 @@ public class HelloController implements Initializable {
             - Sort1 dans lblMove1
             - Sort2 dans lblMove2
             - estUneEvolution dans lblIsEvolution
-            - typePokemon dans lblPokemonType
+            - typePokemon dans lblPokemonType*/
 
-            MOYEN (/2)
-            Utiliser la méthode lblAttribution pour vous aider à réaliser cette tâche, méthode mise à votre disposition en bas de ce fichier
+            /*lblName.setText(c.getNom());
+            lblCardType.setText(c.getCarteType());
+            lblMove1.setText(String.valueOf(c.getSort1()));
+            lblMove2.setText(String.valueOf(c.getSort2()));
+            lblIsEvolution.setText(String.valueOf(c.isEstUneEvolution()));
+            lblPokemonType.setText(c.getTypePokemon());*/
 
-            MOYEN (/3)
+
+
+            /*MOYEN (/2)
+            Utiliser la méthode lblAttribution pour vous aider à réaliser cette tâche, méthode mise à votre disposition en bas de ce fichier*/
+
+            lblAttribution(lblName,c.getNom());
+            lblAttribution(lblCardType,c.getCarteType());
+            lblAttribution(lblMove1,c.getSort1());
+            lblAttribution(lblMove2,c.getSort2());
+            lblAttribution(lblIsEvolution, String.valueOf(c.isEstUneEvolution()));
+            lblAttribution(lblPokemonType,c.getTypePokemon());
+
+            /*MOYEN (/3)
             Utiliser la méthode changeImageViewImg pour afficher la carte correspondant à l'objet. Cette fonction prend
-            uniquement en paramètre l'ImageView imgCard ainsi que l'URL de l'objet (imageUrl qui est un attribut de la classe Carte)
+            uniquement en paramètre l'ImageView imgCard ainsi que l'URL de l'objet (imageUrl qui est un attribut de la classe Carte)*/
 
-            MOYEN-DIFFICILE (/4)
+            changeImageViewImg(imgCard,c.getImageUrl());
+
+
+            /*MOYEN-DIFFICILE (/4)
             Si la carte sélectionnée n'est pas un Pokemon, alors cacher les lignes "Sort 1 :" et "Sort 2:" identifiées respectivement par
             les Label lblLblSort1 et lblLblSort2.
          */
+
+            if (c.getCarteType() != "Pokemon"){
+                changeImageViewImg(imgCard,c.getImageUrl());
+                lblAttribution(lblIsEvolution, String.valueOf(c.isEstUneEvolution()));
+                lblAttribution(lblPokemonType,c.getTypePokemon());
+                lblAttribution(lblName,c.getNom());
+                lblAttribution(lblCardType,c.getCarteType());
+                lblMove1.setText("Impossible");
+                lblMove2.setText("Impossible");
+            }
+
+
 
     }
     @FXML
     public void clickChen(MouseEvent mouseEvent) {
         /*
            FACILE (/2)
-           Cette méthode se lance lorsque je nous cliquons sur la carte "Chen", la carte se trouvant en bas à droite.
+           Cette méthode se lance lorsque nous cliquons sur la carte "Chen", la carte se trouvant en bas à droite.
            Il faudra alors afficher les informations relatives à la carte "Chen" dans l'espace prévu à cet effet.
          */
+        aff(chen);
     }
 
-    public void clickModification(MouseEvent mouseEvent) {
+    public void clickModification(MouseEvent kart) {
         /*
             DIFFICILE (/5)
             Cette fonction devra également s'accompagner de changement dans la vue (SceneBuilder). Vous serez libres de faire comme vous le souhaitez.
             En cliquant sur le bouton modification, vous appliquerez les modifications que vous souhaitez vis-à-vis d'une carte.
 
          */
+        Carte kart ;
+        kart.setNom("");
+        lblName.setText(kart.getNom());
+
+
+
+
     }
+
 
     @FXML
     public void clickPokedex(MouseEvent mouseEvent) {
@@ -163,6 +204,7 @@ public class HelloController implements Initializable {
     }
     public void lblAttribution(Label l, String s)
     {
+
         l.setText(s);
     }
     public void lblAttribution(Label l, int i)
